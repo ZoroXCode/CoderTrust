3b57995b4505fa3f79bc6678227cf3e91bf86c08
ZoroXCode <samirmc8384@outlook.com>
2024-11-30 19:57:08 +0600

    Done

--- components/Animation.js
+++ components/Animation.js
@@ -9,7 +9,7 @@ function Animation({ children }) {
       initial={{ opacity: 0, y: 20 }}
       animate={{ opacity: 1, y: 0 }}
       exit={{ opacity: 0, y: -20 }}
-      transition={{ duration: 0.9, ease: "easeIn" }}
+      transition={{ duration: 0.5, ease: "easeIn" }}
     >
       {children}
     </motion.div>
